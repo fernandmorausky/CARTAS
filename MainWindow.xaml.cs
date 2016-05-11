f1ed415c89464ab0864e26143e98d2bfa4c61303
fernando <fernandmorausky@gmail.com>
2016-05-11 16:22:12 -0500

    aa
    a

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,10 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Cartas
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
     {
 
@@ -28,55 +25,7 @@ namespace Cartas
         public MainWindow()
         {
             InitializeComponent();
-        }
-        //int LoadCards()
-        //{
-        //    int frecuencia1 = 0;
-        //    int frecuencia2 = 0;
-        //    int frecuencia3 = 0;
-        //    int frecuencia4 = 0;
-        //    int frecuencia5 = 0;
-        //    int frecuencia6 = 0;
-        //    int cara;
-        //    Random rd = new Random();
-        //    for (int tiro = 1; tiro <= 6000; tiro++)
-        //    {
-        //        cara = 1 + rd.Next(0,59) % 6;
-        //        switch (cara)
-        //        {
-        //            case 1:
-        //                ++frecuencia1;
-        //                break;
-        //            case 2:
-        //                ++frecuencia2;
-        //                break;
-        //            case 3:
-        //                ++frecuencia3;
-        //                break;
-        //            case 4:
-        //                ++frecuencia4;
-        //                break;
-        //            case 5:
-        //                ++frecuencia5;
-        //                break;
-        //            case 6:
-        //                ++frecuencia6;
-        //                break;
-        //            default:
-        //                 MessageBox.Show("¡El programa no debe llegar hasta aquí!");
-        //                break;
-        //        }
-        //    }
-        //    MessageBox.Show("Cara     Frecuencia");
-        //    MessageBox.Show("\n 1" +frecuencia1 );
-        //    MessageBox.Show("\n 2"  + frecuencia2);
-        //    MessageBox.Show("\n 3" + frecuencia3);
-        //    MessageBox.Show("\n 4" + frecuencia4);
-        //    MessageBox.Show("\n 5" + frecuencia5);
-        //    MessageBox.Show("\n 6" + frecuencia6);
-        //    return 0;
-        //}
-
+        } 
 
         private void btnRepartir_Click(object sender, RoutedEventArgs e)
         {
@@ -98,7 +47,6 @@ namespace Cartas
                 txt2.Text = manos[1].ToString();
                 txt3.Text = manos[2].ToString();
                 txt4.Text = manos[3].ToString();
-
             }
             catch (Exception ex )
             {
@@ -106,5 +54,11 @@ namespace Cartas
                 MessageBox.Show("Message : "+ ex.Message + " StackTrace : " + ex.StackTrace);
             }
         }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+           this.Close();
+        }
+        
     }
 }
